539a6300a0befde49484fcad36f2e230e7853a18
soleplug <30917253+soleplug@users.noreply.github.com>
2017-08-24 18:35:42 -0400

    Update splashuk.py

--- splashuk.py
+++ splashuk.py
@@ -37,7 +37,7 @@ args=("$@")
 if [[ ${args[1]} == *"proxy"* ]]; then
     while true; do
         response=$(curl -x ${args[2]} -c ${args[0]} -s -k -i --compressed "http://www.adidas.com/uk/apps/yeezy/" -H "Host: www.adidas.co.uk" -H "Connection: keep-alive" -H "Cache-Control: max-age=0" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Accept-Encoding: gzip, deflate, sdch" -H "Accept-Language: en-US,en;q=0.8" -H "Cookie: """ + x + """")
-        if [[ $response == *"YEEZY"* ]]; then
+        if [[ $response == *"sitekey"* ]]; then
             echo "Locate cookies in" $args[2]
             echo $args[2] >> 'result.txt'
             break
@@ -46,7 +46,7 @@ if [[ ${args[1]} == *"proxy"* ]]; then
 else
     while true; do
         response=$(curl -c ${args[0]} -s -k -i --compressed "http://www.adidas.com/us/apps/yeezy/" -H "Host: www.adidas.com" -H "Connection: keep-alive" -H "Cache-Control: max-age=0" -H "Upgrade-Insecure-Requests: 1" -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Accept-Encoding: gzip, deflate, sdch" -H "Accept-Language: en-US,en;q=0.8" -H "Cookie: """ + x + """")
-        if [[ $response == *"YEEZY"* ]]; then
+        if [[ $response == *"sitekey"* ]]; then
             echo "Locate cookies in" $args[2]
             echo $args[2] >> 'result.txt'
             break
